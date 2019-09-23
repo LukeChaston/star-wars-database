da98bc38a1b995393fde4a4f6ed0804a7156e36a
git <lukechaston@gmail.com>
2019-09-23 15:42:27 +0100

    called data to front end through setNames function

--- index.php
+++ index.php
@@ -1,12 +1,14 @@
 <?php
 $dbh = new PDO('mysql:host=db;dbname=Luke-Collection', 'root', 'password');
 
-function pullSetNames()
+function pullSetNames($dbh)
 {
-    $query = $db->prepare("SELECT `Name`,`Alignment`,`Force Power (%)`,`Lightsaber Power (%)`,`Blaster Power (%)` FROM `star_wars_characters`");
+    $query = $dbh->prepare("SELECT `Name`,`Alignment`,`Force Power (%)`, `Lightsaber Power (%)`,`Blaster Power (%)` FROM `star_wars_characters`");
    $query->setFetchMode(PDO::FETCH_ASSOC);
    $query->execute();
    $result = $query-> fetchall();
    $sets = $result;
    var_dump($sets);
 }
+
+pullSetNames($dbh);
