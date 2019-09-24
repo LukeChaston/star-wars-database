2389942dc967d2559d22569dec41fb69f8453aa6
git <lukechaston@gmail.com>
2019-09-24 13:45:29 +0100

    Added correct punctuation to loop.

--- functions.php
+++ functions.php
@@ -4,10 +4,10 @@ function cardContainer ($sets)
     $result = '';
     foreach ($sets as $set) {
         $result .= '<div; class=“set”> <h1>' . $set['Name'] . '</h1>
-                   <h2> Alignment ' . $set['Alignment'] . '</h2>
-                   <h2>Force Power (%) ' . $set['Force Power (%)'] .' </h2>
-                   <h2>Lightsaber Power (%) ' . $set['Lightsaber Power (%)'] .' </h2>
-                   <h2>Blaster Power (%) ' . $set['Blaster Power (%)'] .' </h2>
+                   <h2> Alignment: ' . $set['Alignment'] . '</h2>
+                   <h2>Force Power (%): ' . $set['Force Power (%)'] .' </h2>
+                   <h2>Lightsaber Power (%): ' . $set['Lightsaber Power (%)'] .' </h2>
+                   <h2>Blaster Power (%): ' . $set['Blaster Power (%)'] .' </h2>
                    </div>';
     }
     return $result;
