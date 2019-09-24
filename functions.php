f8d257da57cbbebc51e93794e69eb0a5486f003e
git <lukechaston@gmail.com>
2019-09-24 13:39:33 +0100

    Added rest of the data to the foreach loop.

--- functions.php
+++ functions.php
@@ -6,6 +6,8 @@ function cardContainer ($sets)
         $result .= '<div; class=“set”> <h1>' . $set['Name'] . '</h1>
                    <h2> Alignment ' . $set['Alignment'] . '</h2>
                    <h2>Force Power (%) ' . $set['Force Power (%)'] .' </h2>
+                   <h2>Lightsaber Power (%) ' . $set['Lightsaber Power (%)'] .' </h2>
+                   <h2>Blaster Power (%) ' . $set['Blaster Power (%)'] .' </h2>
                    </div>';
     }
     return $result;
