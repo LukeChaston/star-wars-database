31c61f11022a6aead77e963d2fd5eda83b788540
git <lukechaston@gmail.com>
2019-09-24 13:33:44 +0100

    fixed for each loop to better display data.

--- functions.php
+++ functions.php
@@ -1,1 +1,13 @@
 <?php
+function cardContainer ($sets)
+{
+    $result = '';
+    foreach ($sets as $set) {
+        $result .= '<div; class=“set”> <h1>' . $set['Name'] . '</h1>
+                   <h2> Alignment ' . $set['Alignment'] . '</h2>
+                   <h2>Force Power (%) ' . $set['Force Power (%)'] .' </h2>
+                   </div>';
+    }
+    return $result;
+}
+   ?>
