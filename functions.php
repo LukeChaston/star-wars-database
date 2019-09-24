77231ee5103575ec9a4d7fcf533a02c7cf8ef9ce
git <lukechaston@gmail.com>
2019-09-24 15:31:57 +0100

    changes image tag.

--- functions.php
+++ functions.php
@@ -3,13 +3,10 @@ function cardContainer ($sets)
 {
     $result = '';
     foreach ($sets as $set) {
-        $result .= '<div; class=“set”> <h1>' . $set['Name'] . '</h1>
-                   <h2> Alignment: ' . $set['Alignment'] . '</h2>
-                   <h2>Force Power (%): ' . $set['Force Power (%)'] .' </h2>
+        $result .= '<div class=sets> <div class="text"><h1>' . $set['Name'] . '</h1><h2> Alignment: ' . $set['Alignment'] . '</h2><h2>Force Power (%): ' . $set['Force Power (%)'] .' </h2>
                    <h2>Lightsaber Power (%): ' . $set['Lightsaber Power (%)'] .' </h2>
-                   <h2>Blaster Power (%): ' . $set['Blaster Power (%)'] .' </h2>
-                   <img src="Images/' . $set['Image'] .'">
-                   </div>';
+                   <h2>Blaster Power (%): ' . $set['Blaster Power (%)'] .' </h2></div>
+                  <div class="image"> <img src="' . 'Images/' .  $set['Image'] . '"/></div></div>';
     }
     return $result;
 }
