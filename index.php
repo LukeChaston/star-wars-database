58286b14f809f1da8b31c7e9c65217a5457658d1
git <lukechaston@gmail.com>
2019-09-25 15:51:02 +0100

    made suggested changes.

--- index.php
+++ index.php
@@ -1,46 +1,33 @@
 <?php
-$dbh = new PDO('mysql:host=db;dbname=Luke-Collection', 'root', 'password');
 require_once ('functions.php');
-
-/**
- * Function to collect data from the Database and output array.
- *
- * @param $dbh star wars characters data.
- *
- *
- */
-function pullSetNames($dbh)
-{
-    $query = $dbh->prepare("SELECT `Name`,`Alignment`,`Force Power (%)`, `Lightsaber Power (%)`,`Blaster Power (%)`, `Image` FROM `star_wars_characters`");
-   $query->setFetchMode(PDO::FETCH_ASSOC);
-   $query->execute();
-   $result = $query-> fetchall();
-   $sets = $result;
-   return $sets;
-}
-
+$dbh = connectDB();
 $sets = pullSetNames($dbh);
+$result = cardContainer($sets);
 
-?>
+ ?>
+
+
+
 
 <html>
 
 <head>
     <link rel="stylesheet" type="text/css" href="styles.css" />
     <link rel="stylesheet" type="text/css" href="normalize.css"/>
+    <h1>Star Wars Character Table</h1>
 </head>
 
 <body>
-<h1>Star Wars Character Table</h1>
+
+
 <section class="dataContainer">
     <div class="cards">
-<?php echo cardContainer($sets);?>
+        <?php echo $result; ?>
     </div>
 
 
 </section>
 
-<h1>Create your own Character!</h1>
 </body>
 
 </html>
