cb84a7c0c57f5c14816800b8bd878dc5199228a6
git <lukechaston@gmail.com>
2019-09-25 11:04:23 +0100

    modified linme break.

--- functions.php
+++ functions.php
@@ -3,7 +3,8 @@ function cardContainer ($sets)
 {
     $result = '';
     foreach ($sets as $set) {
-        $result .= '<div class=sets> <div class="text"><h1>' . $set['Name'] . '</h1><h2> Alignment: ' . $set['Alignment'] . '</h2><h2>Force Power (%): ' . $set['Force Power (%)'] .' </h2>
+        $result .= '<div class=sets> <div class="text"><h1>' . $set['Name'] . '</h1><h2> Alignment: ' . $set['Alignment'] . '</h2>
+                       <h2>Force Power (%): ' . $set['Force Power (%)'] .' </h2>
                    <h2>Lightsaber Power (%): ' . $set['Lightsaber Power (%)'] .' </h2>
                    <h2>Blaster Power (%): ' . $set['Blaster Power (%)'] .' </h2></div>
                   <div class="image"> <img src="' . 'Images/' .  $set['Image'] . '"/></div></div>';
