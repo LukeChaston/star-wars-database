c6cea5c780aae1fdcf67e6a68125f2325cbefdcb
git <lukechaston@gmail.com>
2019-09-25 15:50:26 +0100

    made suggested changes.

--- functions.php
+++ functions.php
@@ -1,18 +1,40 @@
 <?php
+
+function connectDB () :PDO {
+    $dbh=new PDO ('mysql:host=db;dbname=Luke-Collection', 'root', 'password');
+    return $dbh;
+}
 /**
- * Function to  output data in different card formats.
+ * Function to collect data from the Database and output array.
+ *
+ * @param $dbh star wars characters data.
+ *
  *
  */
-function cardContainer ($sets)
+function pullSetNames($dbh)
 {
-    $result = '';
-    foreach ($sets as $set) {
-        $result .= '<div class=sets> <div class="text"><h1>' . $set['Name'] . '</h1><h2> Alignment: ' . $set['Alignment'] . '</h2>
-                       <h2>Force Power (%): ' . $set['Force Power (%)'] .' </h2>
-                   <h2>Lightsaber Power (%): ' . $set['Lightsaber Power (%)'] .' </h2>
-                   <h2>Blaster Power (%): ' . $set['Blaster Power (%)'] .' </h2></div>
-                  <div class="image"> <img src="' . 'Images/' .  $set['Image'] . '"/></div></div>';
-    }
-    return $result;
+    $query = $dbh->prepare("SELECT `name`,`alignment`,`forcePower(%)`, `lightsaberPower(%)`,`blasterPower(%)`, `image` FROM `star_wars_characters`");
+    $query->setFetchMode(PDO::FETCH_ASSOC);
+    $query->execute();
+    $result = $query->fetchall();
+    $sets = $result;
+    return $sets;
+
 }
-   ?>
+
+    /**
+     * Function to  output data in different card formats.
+     *
+     */
+    function cardContainer($sets)
+    {
+        $result = '';
+        foreach ($sets as $set) {
+            $result .= '<div class=sets> <div class="text"><h1>' . $set['name'] . '</h1><h2> Alignment: ' . $set['alignment'] . '</h2>
+                       <h2>Force Power (%): ' . $set['forcePower(%)'] . ' </h2>
+                   <h2>Lightsaber Power (%): ' . $set['lightsaberPower(%)'] . ' </h2>
+                   <h2>Blaster Power (%): ' . $set['blasterPower(%)'] . ' </h2></div>
+                  <div class="image"> <img src="' . 'Images/' . $set['image'] . '"/></div></div>';
+        }
+        return $result;
+    }
