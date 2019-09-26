79184ab3d3dced6f40bd766e98df61d03948bf1e
git <lukechaston@gmail.com>
2019-09-26 15:23:46 +0100

    changed fields to required.

--- index.php
+++ index.php
@@ -30,12 +30,12 @@ $result = cardContainer($sets);
 <div class="boxes">
 
     <form action="upload.php"  method=post>
-        <p>Name:</p> <input type="text" max="13" name="name"><br>
+        <p>Name:</p> <input type="text" max="13" name="name" required><br>
         <p>Alignment: </p><select name="alignment"><option value="Republic">Republic</option> <option value="Sith">Sith</option><br></select>
-        <p>Lightsaber Power (%):</p> <input type="number" min="1" max="100" name="lightsaberPower"><br>
-        <p>Force Power (%): </p> <input type="number" min="1" max="100"  name="forcePower"><br>
-        <p> Blaster Power (%):</p> <input type="number" min="1" max="100"  name="blasterPower"><br>
-        <p> Upload Avatar Image:</p> <input type="text" name="image"><br>
+        <p>Lightsaber Power (%):</p> <input type="number" min="1" max="100" name="lightsaberPower" required><br>
+        <p>Force Power (%): </p> <input type="number" min="1" max="100"  name="forcePower" required><br>
+        <p> Blaster Power (%):</p> <input type="number" min="1" max="100"  name="blasterPower" required><br>
+        <p> Upload Avatar Image:</p> <input type="text" name="image" required><br>
         <input type="submit" value="Submit">
     </form>
 
